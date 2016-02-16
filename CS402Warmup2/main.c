e600ee9420b2f8aaf50cb424c510f334612b84a0
Gaurav Nijhara <gauravnijhara@MacBook-Pro-2.local>
2016-02-16 00:03:52 -0800

    broadcast condition to kill server threads

--- CS402Warmup2/main.c
+++ CS402Warmup2/main.c
@@ -27,6 +27,9 @@ pthread_mutex_t Q1Mutex;
 pthread_cond_t serverQ;
 unsigned int packetCount = 0;
 unsigned int packetsServed = 0;
+unsigned int serveInterrupt = 0;
+unsigned int isTraceFileMode = 0;
+
 double lambda = 1,mu = 0.35,r = 1.5, b = 10 ,p = 3,num = 20;
 char *fileName;
 pthread_t packetThread,tokenThread,serverThread1,serverThread2,signalQuitThread;
@@ -35,9 +38,13 @@ typedef struct packet
 {
     int ID;
     int tokensNeeded;
+    double interArrivalTime;
+    double serviceTime;
     struct timeval Q1EnterTime,Q2EnterTime,systemTimeOnEnter,systemTimeOnExit,serviceStartTime,serviceEndTime;
     
 }packet;
+
+packet **packetList;
 
 void *packetArrivalMethod(void *args);
 void *tokenArrivalMethod(void *args);
@@ -123,6 +130,9 @@ int main(int argc, const char * argv[]) {
         
             if ((i+1 != argc-1) && (1 == sscanf(argv[i+1],"%s",fileName))) {
                 
+                
+                isTraceFileMode = 1;
+                
                 FILE *fp;
                 struct stat fileCheck;
                 
@@ -142,7 +152,51 @@ int main(int argc, const char * argv[]) {
                                 fprintf(stderr, "Malformed command or file: %s doesn't exist \n",fileName);
                                 exit(1);
                             }
+                            
+                            
+                            char buffer[1026];
+                            int iterator = 0;
+                            while(fgets(buffer, sizeof(buffer)/sizeof(char), fp) != NULL) {
+                                
+                                if (!packetList) {
+                                    
+                                    int tokenCount = atoi(buffer);
+                                    packetList = (packet**)malloc(sizeof(tokenCount));
+                                    
+                                    for (int i = 0; i < tokenCount; i++) {
+                                        packet *newPacket = (packet*)malloc(sizeof(packet));
+                                        newPacket->ID = i+1;
+                                        packetList[i] = newPacket;
+                                    }
+                                    
+                                }
+                                else {
+                                    
+                                    // parsing code
+                                    const char s[2] = "\t";
+                                    char *token;
+                                    
+                                    // inter-arrival time
+                                    token = strtok(buffer, s);
+                                    double interArrivalTime = atoll(token);
+                                    packetList[iterator]->interArrivalTime = interArrivalTime;
+                                    
+                                    // tokens needed
+                                    token = strtok(NULL, s);
+                                    int tokensNeeded = atoi(token);
+                                    packetList[iterator]->tokensNeeded = tokensNeeded;
+                                    
+                                    // service time
+                                    token = strtok(NULL, s);
+                                    double serviceTime = atoll(token);
+                                    packetList[iterator]->serviceTime = serviceTime;
+                                    
+                                }
+                                iterator++;
+                            }
+
                             // file opened here
+                            fclose(fp);
                         }
                     }
                     else if( fileCheck.st_mode & S_IFDIR )
@@ -174,6 +228,18 @@ int main(int argc, const char * argv[]) {
         }
     }
     
+    if (!isTraceFileMode) {
+        
+        for (int i = 0; i < num; i++) {
+            packet *newPacket = (packet*)malloc(sizeof(packet));
+            newPacket->ID = i+1;
+            newPacket->interArrivalTime = (1/lambda);
+            newPacket->tokensNeeded = p;
+            newPacket->serviceTime = (1/mu);
+            packetList[i] = newPacket;
+        }
+    }
+    
     sigemptyset(&quitSignal);
     sigaddset(&quitSignal, SIGINT);
     pthread_sigmask(SIG_BLOCK, &quitSignal, NULL);
@@ -189,7 +255,7 @@ int main(int argc, const char * argv[]) {
     pthread_join(tokenThread, NULL);
     pthread_join(serverThread1, NULL);
     pthread_join(serverThread2, NULL);
-    
+    pthread_join(signalQuitThread,NULL);
     
     return 0;
 }
@@ -201,16 +267,15 @@ void *packetArrivalMethod(void *args)
     
     while (packetCount < num) {
         
-        usleep((unsigned int)fabs((1/lambda)*1000000L - elapsedTime/1000000L));
+        usleep((unsigned int)fabs((packetList[packetCount]->interArrivalTime)*1000000L - elapsedTime/1000000L));
         
         gettimeofday(&time,NULL);
         currentTime = time.tv_sec + time.tv_usec*1000000L;
         
-        packet *newPacket = (packet*)malloc(sizeof(packet));
+
+        packet *newPacket = packetList[packetCount++];
         
-        newPacket->tokensNeeded = p;
-        newPacket->ID = ++packetCount;
-        
+        // packetCount
         printf("\n packet%d arrives , need %d tokens, inter-arrival time = %lld",newPacket->ID,newPacket->tokensNeeded,(currentTime-prevTokenArrivalTime));
         
         prevTokenArrivalTime = currentTime;
@@ -365,11 +430,11 @@ void *serverMethod(void *args)
         pthread_mutex_lock(&Q1Mutex);
         
 
-        while (My402ListEmpty(&Q2) && packetsServed != num) {
+        while (My402ListEmpty(&Q2) && packetsServed != num && !serveInterrupt) {
             pthread_cond_wait(&serverQ, &Q1Mutex);
         }
         
-        if (packetsServed == num) {
+        if (packetsServed == num || serveInterrupt) {
             pthread_mutex_unlock(&Q1Mutex);
             break;
         }
@@ -398,7 +463,10 @@ void *serverMethod(void *args)
 
         printf("\n p% departs from S1, service time = %lldms , time in system = %ldms",dequePacket->ID,(currentTime - (dequePacket->serviceStartTime.tv_sec + dequePacket->serviceStartTime.tv_usec*1000)),(dequePacket->serviceStartTime.tv_sec + dequePacket->serviceStartTime.tv_usec*1000000L) - (dequePacket->serviceEndTime.tv_sec + dequePacket->serviceEndTime.tv_usec*1000000L));
         
-        if (packetsServed == num) {
+        if (packetsServed == num || serveInterrupt) {
+            if (packetsServed == num) {
+                pthread_cond_broadcast(&serverQ);
+            }
             break;
         }
        
@@ -414,11 +482,11 @@ void *server2Method(void *args)
         
         pthread_mutex_lock(&Q1Mutex);
         
-        while (My402ListEmpty(&Q2) && packetsServed != num) {
+        while (My402ListEmpty(&Q2) && packetsServed != num && !serveInterrupt) {
             pthread_cond_wait(&serverQ, &Q1Mutex);
         }
         
-        if (packetsServed == num) {
+        if (packetsServed == num || serveInterrupt) {
             pthread_mutex_unlock(&Q1Mutex);
             break;
         }
@@ -447,7 +515,10 @@ void *server2Method(void *args)
         
            printf("\n p% departs from S2, service time = %lldms , time in system = %ldms",dequePacket->ID,(currentTime - (dequePacket->serviceStartTime.tv_sec + dequePacket->serviceStartTime.tv_usec*1000)),(dequePacket->serviceStartTime.tv_sec + dequePacket->serviceStartTime.tv_usec*1000000L) - (dequePacket->serviceEndTime.tv_sec + dequePacket->serviceEndTime.tv_usec*1000000L));
 
-        if (packetsServed == num) {
+        if (packetsServed == num || serveInterrupt) {
+            if (packetsServed == num) {
+                pthread_cond_broadcast(&serverQ);
+            }
             break;
         }
         
@@ -461,7 +532,7 @@ void *handleQuitGracefully(void *args)
         pthread_sigmask(SIG_UNBLOCK, &quitSignal, NULL);
         action.sa_handler = handleQuit;
         sigaction(SIGINT, &action, NULL);
-        usleep(3000000);
+        usleep(100000000);
     }
 }
 
@@ -471,6 +542,8 @@ void handleQuit(int signal)
     pthread_cancel(packetThread);
     
     // add a bool to condition and broadcast condition
+    serveInterrupt = 1;
+    pthread_cond_broadcast(&serverQ);
     
     pthread_mutex_lock(&Q1Mutex);
     
