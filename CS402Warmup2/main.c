ed149b982a6270e84093fd74924a0004bd16b686
Gaurav Nijhara <contact@gauravnijhara.com>
2016-02-17 01:50:39 -0800

    print statements

--- CS402Warmup2/main.c
+++ CS402Warmup2/main.c
@@ -505,6 +505,9 @@ void *serverMethod(void *args)
         pthread_mutex_unlock(&Q1Mutex); 
         
         gettimeofday(&dequePacket->serviceStartTime,NULL);
+        
+        printf("start time tv_sec %ld  tv_usec %ld",dequePacket->serviceStartTime.tv_sec,dequePacket->serviceStartTime.tv_sec);
+        
         elapsedTime = dequePacket->serviceStartTime.tv_sec*1000000 + dequePacket->serviceStartTime.tv_usec - currentTime;
 
         printf("my service start time is %ld , elapsed time is %lld , currenttime %lld\n",(dequePacket->serviceStartTime.tv_sec*1000000 + dequePacket->serviceStartTime.tv_usec),elapsedTime,currentTime);
@@ -578,6 +581,9 @@ void *server2Method(void *args)
         pthread_mutex_unlock(&Q1Mutex); 
         
         gettimeofday(&dequePacket->serviceStartTime,NULL);
+        
+        printf("start time tv_sec %ld  tv_usec %ld",dequePacket->serviceStartTime.tv_sec,dequePacket->serviceStartTime.tv_sec);
+
         elapsedTime = (dequePacket->serviceStartTime.tv_sec*1000000 + dequePacket->serviceStartTime.tv_usec) - currentTime;
         
         printf("my service start time is %ld , elapsed time is %lld , currenttime %lld\n",(dequePacket->serviceStartTime.tv_sec*1000000 + dequePacket->serviceStartTime.tv_usec),elapsedTime,currentTime);
